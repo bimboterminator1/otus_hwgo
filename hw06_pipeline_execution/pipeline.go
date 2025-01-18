98c394b821d601f6aed8a0604939913621eab711
Alexander Kondakov <a.kondakov@arenadata.io>
2025-01-18 11:29:09 +0300

    HW6 Pipeline

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,39 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		return in
+	}
+
+	proxyChan := func(inChan In, doneChan In) Out {
+		out := make(Bi)
+
+		go func() {
+			defer func() {
+				close(out)
+				//nolint:revive
+				for range inChan {
+				}
+			}()
+			for {
+				select {
+				case <-doneChan:
+					return
+				case v, ok := <-inChan:
+					if !ok {
+						return
+					}
+					out <- v
+				}
+			}
+		}()
+		return out
+	}
+
+	outChan := in
+	for i := 0; i < len(stages); i++ {
+		outChan = stages[i](proxyChan(outChan, done))
+	}
+
+	return outChan
 }
