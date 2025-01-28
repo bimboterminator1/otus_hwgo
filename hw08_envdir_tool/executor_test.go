0806b7214f1053008e7977e4e6fc1862e2d39125
Alexander Kondakov <a.kondakov@arenadata.io>
2025-01-28 21:33:14 +0300

    HW08 envdir tool

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,24 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("normal run", func(t *testing.T) {
+		os.Setenv("HELLO", "dummy")
+		env := make(Environment)
+		env["HELLO"] = EnvValue{
+			"HELLO",
+			false,
+		}
+		rc := RunCmd([]string{"/bin/bash", "./testdata/echo.sh", "arg"}, env)
+		require.Equal(t, 0, rc)
+		evar, ok := os.LookupEnv("HELLO")
+		require.True(t, ok)
+		require.Equal(t, "HELLO", evar)
+	})
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		fmt.Fprintf(os.Stderr, "%s : wrong arguments\n", os.Args[0])
+		os.Exit(1)
+	}
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s : %s\n", os.Args[0], err)
+		os.Exit(1)
+	}
+	rc := RunCmd(os.Args[2:], env)
+	os.Exit(rc)
 }
